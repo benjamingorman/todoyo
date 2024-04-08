19b99a2c1b94ae426344f634801d8d8c84e418c6
Benjamin Gorman <8076bgorman@gmail.com>
2024-04-08 14:13:52 +0100

    Better title bar display

--- src/App.tsx
+++ src/App.tsx
@@ -1,156 +1,173 @@
-import { For, createSignal, createEffect, Show } from "solid-js";
-import { TaskData, TaskWidget, TaskList } from "./TodoTask";
+import { For, createEffect, createSignal } from "solid-js";
 import { createStore } from "solid-js/store";
 import { Store } from "tauri-plugin-store-api";
-import { TodoTitle } from "./TodoTitle";
-import { MessageBroker } from "./MessageBroker";
 import { AppApi } from "./AppApi";
 import { configureDefaultHandlers } from "./Handlers";
+import { MessageBroker } from "./MessageBroker";
+import { TaskData, type TaskList, TaskWidget } from "./TodoTask";
+import { TodoTitle } from "./TodoTitle";
 import { UsageInfo } from "./UsageInfo";
 
 interface SaveData {
-  taskLists: TaskList[];
-  selectedListIndex: number;
-  selectedIndex: number;
+	taskLists: TaskList[];
+	selectedListIndex: number;
+	selectedIndex: number;
 }
 
 const SETTINGS_VERSION = 1;
 let DONE_LOAD_TASKS = false;
 
 function App() {
-  const initTask = new TaskData();
-  initTask.title = "New task";
-  const initList: TaskList = { title: "Tasks", tasks: [initTask], done: false };
-  const [taskLists, setTaskLists] = createStore<TaskList[]>([initList]);
-  const [selectedListIndex, setSelectedListIndex] = createSignal(0);
-  // -1 means no task is selected and we're editing the list title
-  const [selectedIndex, setSelectedIndex] = createSignal(-1);
-  const [doFocus, setDoFocus] = createSignal(false);
-  const [showHelp, setShowHelp] = createSignal(true);
-  const store = new Store(`.tasks.${SETTINGS_VERSION}.dat`);
+	const initTask = new TaskData();
+	initTask.title = "New task";
+	const initList: TaskList = { title: "Tasks", tasks: [initTask], done: false };
+	const [taskLists, setTaskLists] = createStore<TaskList[]>([initList]);
+	const [selectedListIndex, setSelectedListIndex] = createSignal(0);
+	// -1 means no task is selected and we're editing the list title
+	const [selectedIndex, setSelectedIndex] = createSignal(-1);
+	const [doFocus, setDoFocus] = createSignal(false);
+	const [showHelp, setShowHelp] = createSignal(true);
+	const store = new Store(`.tasks.${SETTINGS_VERSION}.dat`);
 
-  let todoListsRef: HTMLDivElement | undefined;
+	let todoListsRef: HTMLDivElement | undefined;
 
-  const api = new AppApi({
-    getTaskLists: () => taskLists,
-    setTaskLists,
-    selectedListIndex,
-    setSelectedListIndex,
-    selectedIndex,
-    setSelectedIndex,
-    doFocus,
-    setDoFocus,
-    showHelp,
-    setShowHelp,
-  });
+	const api = new AppApi({
+		getTaskLists: () => taskLists,
+		setTaskLists,
+		selectedListIndex,
+		setSelectedListIndex,
+		selectedIndex,
+		setSelectedIndex,
+		doFocus,
+		setDoFocus,
+		showHelp,
+		setShowHelp,
+	});
 
-  const broker = new MessageBroker(api);
-  broker.setupHotkeys();
-  configureDefaultHandlers(api, broker);
+	const broker = new MessageBroker(api);
+	broker.setupHotkeys();
+	configureDefaultHandlers(api, broker);
 
-  createEffect(() => {
-    if (!todoListsRef) return;
-    const listRef = document.getElementById(`todo-list-${selectedListIndex()}`);
-    console.log("listRef", listRef);
-    if (!listRef) return;
-    console.log("offsetLeft", listRef.offsetLeft);
-    console.log("scrollLeft", todoListsRef.scrollLeft);
-    todoListsRef.scrollTo({
-      left: listRef.offsetLeft,
-      behavior: "smooth",
-    })
-  });
+	createEffect(() => {
+		if (!todoListsRef) return;
+		const listRef = document.getElementById(`todo-list-${selectedListIndex()}`);
+		console.log("listRef", listRef);
+		if (!listRef) return;
+		console.log("offsetLeft", listRef.offsetLeft);
+		console.log("scrollLeft", todoListsRef.scrollLeft);
+		todoListsRef.scrollTo({
+			left: listRef.offsetLeft,
+			behavior: "smooth",
+		});
+	});
 
-  // Save/load tasks from store
-  createEffect(() => {
-    const saveData: SaveData = {
-      taskLists: taskLists,
-      selectedListIndex: selectedListIndex(),
-      selectedIndex: selectedIndex(),
-    };
-    console.log("Saving tasks...", saveData);
-    // Use different save data for dev and prod
-    store.set(`saveData-${window.location.host}`, saveData);
-    store.save();
-  });
+	// Save/load tasks from store
+	createEffect(() => {
+		const saveData: SaveData = {
+			taskLists: taskLists,
+			selectedListIndex: selectedListIndex(),
+			selectedIndex: selectedIndex(),
+		};
+		console.log("Saving tasks...", saveData);
+		// Use different save data for dev and prod
+		store.set(`saveData-${window.location.host}`, saveData);
+		store.save();
+	});
 
-  if (!DONE_LOAD_TASKS) {
-    store.get(`saveData-${window.location.host}`).then(
-      (data) => {
-        if (!data) {
-          console.error("No save data found in store");
-          return;
-        }
+	if (!DONE_LOAD_TASKS) {
+		store.get(`saveData-${window.location.host}`).then(
+			(data) => {
+				if (!data) {
+					console.error("No save data found in store");
+					return;
+				}
 
-        const saveData = data as SaveData;
-        if (!saveData.taskLists) {
-          console.error("No lists found in saveData");
-          return;
-        };
+				const saveData = data as SaveData;
+				if (!saveData.taskLists) {
+					console.error("No lists found in saveData");
+					return;
+				}
 
-        setTaskLists(saveData.taskLists);
-        setSelectedListIndex(saveData.selectedListIndex);
-        setSelectedIndex(saveData.selectedIndex);
-      },
-      (e) => {
-        console.error("No tasks found in store", e);
-      }
-    );
-    DONE_LOAD_TASKS = true;
-  }
+				setTaskLists(saveData.taskLists);
+				setSelectedListIndex(saveData.selectedListIndex);
+				setSelectedIndex(saveData.selectedIndex);
+			},
+			(e) => {
+				console.error("No tasks found in store", e);
+			},
+		);
+		DONE_LOAD_TASKS = true;
+	}
 
-  return (
-    <div>
-      <h1>
-        todo<span class="yo-accent">yo.</span>
-      </h1>
+	return (
+		<div id="App">
+			<h1>
+				todo<span class="yo-accent">yo.</span>
+			</h1>
 
-      <div ref={todoListsRef} class="todo-lists">
-        <For each={taskLists}>{(currList, i) => (
-          <div id={`todo-list-${i()}`} class="container">
-            <div class="row">
-              <TodoTitle
-                text={currList.title}
-                done={currList.done}
-                selected={i() === selectedListIndex() && selectedIndex() === -1}
-                doFocus={i() === selectedListIndex() && selectedIndex() === -1 && doFocus()}
-                onEdit={(...args) => { api.onEditListTitle(i(), ...args) }}
-              />
-            </div>
+			<div ref={todoListsRef} class="todo-lists">
+				<For each={taskLists}>
+					{(currList, i) => (
+						<div id={`todo-list-${i()}`} class="container">
+							<div class="row">
+								<TodoTitle
+									text={currList.title}
+									done={currList.done}
+									selected={
+										i() === selectedListIndex() && selectedIndex() === -1
+									}
+									doFocus={
+										i() === selectedListIndex() &&
+										selectedIndex() === -1 &&
+										doFocus()
+									}
+									onEdit={(...args) => {
+										api.onEditListTitle(i(), ...args);
+									}}
+								/>
+							</div>
 
-            <div class="row">
-              <For each={currList.tasks}>
-                {(task, j) => (
-                  <TaskWidget
-                    task={task}
-                    index={j()}
-                    selected={i() === selectedListIndex() && j() === selectedIndex()}
-                    doFocus={i() === selectedListIndex() && j() === selectedIndex() && doFocus()}
-                    onEditTaskTitle={(...args) => api.onEditTaskTitle(...args)}
-                    onTouch={() => {
-                      setSelectedIndex(j())
-                      setSelectedListIndex(i())
-                    }}
-                  />
-                )}
-              </For>
-            </div>
+							<div class="row">
+								<For each={currList.tasks}>
+									{(task, j) => (
+										<TaskWidget
+											task={task}
+											index={j()}
+											selected={
+												i() === selectedListIndex() && j() === selectedIndex()
+											}
+											doFocus={
+												i() === selectedListIndex() &&
+												j() === selectedIndex() &&
+												doFocus()
+											}
+											onEditTaskTitle={(...args) =>
+												api.onEditTaskTitle(...args)
+											}
+											onTouch={() => {
+												setSelectedIndex(j());
+												setSelectedListIndex(i());
+											}}
+										/>
+									)}
+								</For>
+							</div>
 
-            <div class="row">
-              <button class="add-task" onClick={() => api.createNewTask()}>
-                +
-              </button>
-            </div>
-          </div>
-        )}</For>
-        {/* Ghost container to make sure scrolling always works */}
-        <div class="container"></div>
+							<div class="row">
+								<button class="add-task" onClick={() => api.createNewTask()}>
+									+
+								</button>
+							</div>
+						</div>
+					)}
+				</For>
+				{/* Ghost container to make sure scrolling always works */}
+				<div class="container" />
+			</div>
 
-      </div>
-
-      <UsageInfo toggle={showHelp()} />
-    </div>
-  );
+			<UsageInfo toggle={showHelp()} />
+		</div>
+	);
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,25 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
 
+import { appWindow } from "@tauri-apps/api/window";
+import App from "./App";
+import "./assets/fonts/PatrickHand-Regular.ttf";
 import "./css/pico.colors.min.css";
 // import "./css/pico.amber.css";
 import "./fonts.css";
-import "./styles.css";
 // import "./theme-tomnook.css";
-import "./styles-titlebar.css";
-import "./assets/fonts/PatrickHand-Regular.ttf";
-import App from "./App";
-import { appWindow } from "@tauri-apps/api/window";
+import "./styles-titlebar.scss";
+import "./styles.css";
 
 document
-  .getElementById("titlebar-minimize")
-  ?.addEventListener("click", () => appWindow.minimize());
+	.getElementById("titlebar-minimize")
+	?.addEventListener("click", () => appWindow.minimize());
 
 document
-  .getElementById("titlebar-maximize")
-  ?.addEventListener("click", () => appWindow.toggleMaximize());
+	.getElementById("titlebar-maximize")
+	?.addEventListener("click", () => appWindow.toggleMaximize());
 document
-  .getElementById("titlebar-close")
-  ?.addEventListener("click", () => appWindow.close());
+	.getElementById("titlebar-close")
+	?.addEventListener("click", () => appWindow.close());
 
 render(() => <App />, document.getElementById("root") as HTMLElement);
